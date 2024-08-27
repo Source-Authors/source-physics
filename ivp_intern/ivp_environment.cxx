fe77b30907fc042a9f20a152035b8a26867ae043
Dmitry Tsarevich <dimhotepus@gmail.com>
2024-08-27 09:27:42 +0300

    ivp_intern: Mark assigned for template instantiation var as unused

--- ivp_intern/ivp_environment.cxx
+++ ivp_intern/ivp_environment.cxx
@@ -799,7 +799,7 @@ void ivp_dummy_func(){
     IVP_U_Set_Active<IVP_Core> ivp_class_dummy1(16);
     IVP_U_Set_Active<IVP_Real_Object> ivp_class_dummy2(16);
     IVP_U_Set_Active<IVP_Mindist_Base> ivp_class_dummy3(16);
-    IVP_Attacher_To_Cores<IVP_Controller_Buoyancy> *ivp_class_dummy8 = new IVP_Attacher_To_Cores<IVP_Controller_Buoyancy>(NULL);
+    [[maybe_unused]] IVP_Attacher_To_Cores<IVP_Controller_Buoyancy> *ivp_class_dummy8 = new IVP_Attacher_To_Cores<IVP_Controller_Buoyancy>(NULL);
 }
 
 void IVP_Time_Event_D::simulate_time_event(IVP_Environment *env) {
