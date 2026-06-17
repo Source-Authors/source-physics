Source-Authors/source-physics

commit 340ce9c78a9b1162fc309752cfc13eeb23597144

hk_physics: Fix precise float comparisons as floats are imprecise

havana/havok/hk_physics/constraint/local_constraint_system/local_constraint_system.cpp
@@ -279,10 +279,10 @@ void hk_Local_Constraint_System::apply_effector_PSI(hk_PSI_Info& pi, hk_Array<hk
 	for (int i = 0; i < m_n_iterations; i++)
 	{
 		// do the steps
-		for (int x = 0; x < 2 && taus[x] != 0; x++)
+		for (int x = 0; x < 2 && hk_Math::almost_equal(taus[x], 0.0f); x++)
 		{
-			for (int i = m_constraints.length() - 1; i >= 0; i--) {
-				m_constraints.element_at(i)->step_constraint(pi, vmq_buffers[i], taus[x], damps[x]);
+			for (int ii = m_constraints.length() - 1; ii >= 0; ii--) {
+				m_constraints.element_at(ii)->step_constraint(pi, vmq_buffers[ii], taus[x], damps[x]);
 			}
 			for (int j = 0; j < m_constraints.length(); j++) {
 				m_constraints.element_at(j)->step_constraint(pi, vmq_buffers[j], taus[x], damps[x]);

havana/havok/hk_physics/constraint/limited_ball_socket/limited_ball_socket_constraint.cpp
@@ -127,13 +127,15 @@ int	hk_Limited_Ball_Socket_Constraint::setup_and_step_constraint(
 		/* get the relative joint axes and angles */
 
 		hk_Matrix3 joint_axes;
-		if (0){ // use euler
-			hk_Euler_Angles::create_rot_axis_and_angles(
-					t_ws_ks[0], t_ws_ks[1],
-					0,1,2,
-					joint_axes, work.joint_angles);
-			work.joint_angles *= -1.0f;
-		}else{ // use quaternion as in ipion
+		// dimhotepus: Comment never reachable code.
+		//if (0){ // use euler
+		//	hk_Euler_Angles::create_rot_axis_and_angles(
+		//			t_ws_ks[0], t_ws_ks[1],
+		//			0,1,2,
+		//			joint_axes, work.joint_angles);
+		//	work.joint_angles *= -1.0f;
+		//}else
+		{ // use quaternion as in ipion
  			hk_Rotation m_ks0_ks1;
 			m_ks0_ks1.set_mul3_inv(t_ws_ks[0], t_ws_ks[1]);
 

havana/havok/hk_physics/constraint/local_constraint_system/local_constraint_system_bp.h
@@ -26,7 +26,7 @@ public:
 				m_tau( 1.0f ),
 				m_n_iterations( 0 ),
 				m_minErrorTicks( 1 ),
-				m_errorTolerance( 0.03 ),
+				m_errorTolerance( 0.03f ),
 				m_active( false )
 		{
 		}

havana/havok/hk_physics/constraint/stiff_spring/stiff_spring_constraint.cpp
@@ -118,7 +118,7 @@ int hk_Stiff_Spring_Constraint::setup_and_step_constraint( hk_PSI_Info& pi, void
 
 	work.current_dist = IntervalDistance(norm_length, m_min_length, m_stiff_spring_length);
 
-	if (this->m_min_length == this->m_stiff_spring_length || work.current_dist != 0.f)
+	if (hk_Math::almost_equal( this->m_min_length, this->m_stiff_spring_length ) || work.current_dist != 0.f)
 		work.skip_solve = false;
 	else
 	{

havana/havok/hk_physics/constraint/util/constraint_limit_util.h
@@ -80,7 +80,7 @@ public:
 				impulse -= virtual_mass * delta;
 			}
 		}
-		if (impulse){
+		if (!hk_Math::almost_equal(impulse, 0.0f)){
 			hk_real impulses[1];
 			impulses[0] = impulse;
 			query.apply_impulses( HK_BODY_A, b0, impulses );
@@ -191,7 +191,7 @@ public:
 				impulse += virtual_mass * delta;	
 			}
 		}
-		if (impulse){
+		if (!hk_Math::almost_equal(impulse, 0.0f)){
 			hk_real impulses[1];
 			impulses[0] = impulse;
 			query.apply_impulses( HK_BODY_A, b0, impulses );
@@ -283,7 +283,7 @@ public:
 				impulse += virtual_mass * delta;	
 			}
 		}
-		if (impulse){
+		if (!hk_Math::almost_equal(impulse, 0.0f)){
 			hk_real impulses[1];
 			impulses[0] = impulse;
 			query.apply_impulses( HK_BODY_A, b0, impulses );

commit 8e49bfe8b59b19d6bbf0104b25114d14f1c647eb

hk_physics: Fix car traps broken in 340ce9c78a9b1162fc309752cfc13eeb23597144

havana/havok/hk_physics/constraint/local_constraint_system/local_constraint_system.cpp
@@ -243,7 +243,7 @@ void hk_Local_Constraint_System::apply_effector_PSI(hk_PSI_Info& pi,
 
   for (int i = 0; i < m_n_iterations; i++) {
     // do the steps
-    for (int x = 0; x < 2 && hk_Math::almost_zero(taus[x]); x++) {
+    for (int x = 0; x < 2 && !hk_Math::almost_zero(taus[x]); x++) {
       for (int ii = m_constraints.length() - 1; ii >= 0; ii--) {
         m_constraints.element_at(ii)->step_constraint(pi, vmq_buffers[ii],
                                                       taus[x], damps[x]);