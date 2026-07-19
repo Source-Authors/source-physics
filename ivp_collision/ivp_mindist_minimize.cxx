Source-Authors/source-physics

commit 5a47459b729dcdd4ca01b1a5461d019c2a6f566d

ivp_collision: Ensure no out of bounds read if loop_hash_size reached its limit

ivp_collision/ivp_mindist_minimize.cxx
@@ -87,6 +87,11 @@ IVP_BOOL IVP_Mindist_Minimize_Solver::check_loop_hash(
   IVP_ASSERT(i_s0 < 4);
   IVP_ASSERT(i_s1 < 4);
 
+  // dimhotepus: Early return on loop instead of out-of-bounds.
+  if (loop_hash_size >= IVP_LOOP_LIST_SIZE) {
+    return IVP_TRUE;
+  }
+
   hk_intp x0 = hk_intp(i_e0) | i_s0;
   hk_intp x1 = hk_intp(i_e1) | i_s1;
 
@@ -103,10 +108,6 @@ IVP_BOOL IVP_Mindist_Minimize_Solver::check_loop_hash(
     if (s->a == x0 && s->b == x1) {
       return IVP_TRUE;
     }
-  }
-
-  if (loop_hash_size >= IVP_LOOP_LIST_SIZE) {
-    return IVP_TRUE;
   }
 
   loop_hash[loop_hash_size] = {x0, x1};

commit ecd3bb5aff98fea976f7b9579caa308f8d5ce852

ivp_collision: Count last loop hash when search for one (regression from 5a47459b729dcdd4ca01b1a5461d019c2a6f566d)

ivp_collision/ivp_mindist_minimize.cxx
@@ -88,8 +88,10 @@ IVP_BOOL IVP_Mindist_Minimize_Solver::check_loop_hash(
   IVP_ASSERT(i_s0 < 4);
   IVP_ASSERT(i_s1 < 4);
 
+  // dimhotepus: Precache for performance.
+  const int loop_hash_len{loop_hash_size};
   // dimhotepus: Early return on loop instead of out-of-bounds.
-  if (loop_hash_size >= IVP_LOOP_LIST_SIZE) {
+  if (loop_hash_len > std::size(loop_hash)) {
     return IVP_TRUE;
   }
 
@@ -101,9 +103,9 @@ IVP_BOOL IVP_Mindist_Minimize_Solver::check_loop_hash(
     std::swap(x0, x1);
   }
 
-  IVP_MM_Loop_Hash_Struct *s = &loop_hash[loop_hash_size];
+  IVP_MM_Loop_Hash_Struct *s = &loop_hash[loop_hash_len];
   // dimhotepus: Fix out of bounds read from loop_hash.
-  for (int i = loop_hash_size; i > 0; i--) {
+  for (int i = loop_hash_len; i > 0; i--) {
     s--;
 
     if (s->a == x0 && s->b == x1) {
@@ -111,7 +113,12 @@ IVP_BOOL IVP_Mindist_Minimize_Solver::check_loop_hash(
     }
   }
 
-  loop_hash[loop_hash_size] = {x0, x1};
+  // dimhotepus: Fix out of bounds write from loop_hash.
+  if (loop_hash_len >= std::size(loop_hash)) {
+    return IVP_TRUE;
+  }
+
+  loop_hash[loop_hash_len] = {x0, x1};
   loop_hash_size++;
   return IVP_FALSE;
 }